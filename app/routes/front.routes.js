283f456f138c7f838976ba194e7debcae00b9bc6
Designs Encoded <67951871+DesignsEncoded@users.noreply.github.com>
2021-06-29 17:36:34 +0530

    Delete app/routes directory

--- app/routes/front.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-router.get('/', (req, res) => res.render('front/home'))
-router.get('/about-us', (req, res) => res.render('front/about'))
-router.get('/contact-us', (req, res) => res.render('front/contact'))
-
-module.exports = router
